4acdf8442a926321ebdb1512301acaec0015d4f0
Andrey Lizunov <anlizev@yandex-team.ru>
2021-02-07 11:30:39 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Andrey Lizunov on 2/7/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
