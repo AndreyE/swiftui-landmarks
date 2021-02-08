83d13b474b6fe501f3696a27169a3e106e39b0ed
Andrey Lizunov <anlizev@yandex-team.ru>
2021-02-08 18:38:55 +0300

    Multiple devices preview

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -23,6 +23,10 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone XS Max", "iPhone SE (2nd generation)", "iPad Pro (12.9-inch) (2nd generation)"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
