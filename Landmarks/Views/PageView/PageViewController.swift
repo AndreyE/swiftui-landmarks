2ee63be11432f599870118abe0efd3b134e6c914
Andrey Lizunov <anlizev@yandex-team.ru>
2021-03-16 09:32:24 +0300

    Create the View Controller’s Data Source
    A SwiftUI view that represents a UIKit view controller can define a Coordinator type that SwiftUI manages and provides as part of the representable view’s context.
    
    1. Declare a nested Coordinator class inside PageViewController.
    2. Add another method to PageViewController to make the coordinator.
       SwiftUI calls this makeCoordinator() method before makeUIViewController(context:)
    3. Initialize an array of controllers in the coordinator using the pages array of views.
    4. Add UIPageViewControllerDataSource conformance to the Coordinator type
    5. Add the coordinator as the data source of the UIPageViewController.

--- Landmarks/Views/PageView/PageViewController.swift
+++ Landmarks/Views/PageView/PageViewController.swift
@@ -11,16 +11,62 @@ import UIKit
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
     var pages: [Page]
     
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
     func makeUIViewController(context: Context) -> UIPageViewController {
         let pageViewController = UIPageViewController(
             transitionStyle: .scroll,
             navigationOrientation: .horizontal)
+        pageViewController.dataSource = context.coordinator
         
         return pageViewController
     }
     
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
         pageViewController.setViewControllers(
-            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+            [context.coordinator.controllers[0]], direction: .forward, animated: true)
+    }
+    
+    // Tip
+    // You can use this coordinator to implement common Cocoa patterns, such as delegates, data sources, and responding to user events via target-action.
+    class Coordinator: NSObject, UIPageViewControllerDataSource {
+        var parent: PageViewController
+        // The coordinator is a good place to store these controllers, because the system initializes them only once, and before you need them to update the view controller.
+        var controllers = [UIViewController]()
+        
+        init(_ pageViewController: PageViewController) {
+            parent = pageViewController
+            controllers = parent.pages.map { UIHostingController(rootView: $0) }
+        }
+        
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            
+            if index == 0 {
+                return controllers.last
+            } else {
+                return controllers[index - 1]
+            }
+        }
+        
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            
+            if index + 1 == controllers.count {
+                return controllers.first
+            } else {
+                return controllers[index + 1]
+            }
+        }
     }
 }
